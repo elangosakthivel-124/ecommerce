8d33538719114ba05b49cb7dc8d36e401bff04c5
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-10 21:12:03 +0530

    Add product and cart related URL paths

--- urls.py
+++ urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('product/<int:id>/', views.product_detail, name='product'),
+    path('add/<int:id>/', views.add_to_cart, name='add'),
+    path('cart/', views.cart, name='cart'),
+    path('remove/<int:id>/', views.remove_cart, name='remove'),
+
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
