451e7389a863806d72e2bdc2e55d4a9e5ab3d342
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-14 22:39:48 +0530

    Update admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Product, Order, OrderItem
+from .models import Product, Order, OrderItem, Profile, ShippingAddress
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'total', 'status', 'created')
+    inlines = [OrderItemInline]
+
 
 admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Profile)
+admin.site.register(ShippingAddress)
