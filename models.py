c4abb69c382cb27254dfa1cb9ed62a11f2e014a5
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-14 22:39:31 +0530

    Update models.py

--- models.py
+++ models.py
@@ -44,3 +44,18 @@ class OrderItem(models.Model):
     def __str__(self):
         return f"Order {self.id}"
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class ShippingAddress(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+
