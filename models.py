df4e5849659b358c8e91038d20c9fcf201072688
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-15 22:23:10 +0530

    Update models.py

--- models.py
+++ models.py
@@ -59,3 +59,20 @@ class ShippingAddress(models.Model):
     phone = models.CharField(max_length=15)
     address = models.TextField()
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
