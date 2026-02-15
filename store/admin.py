24eca401f3149a7160314718f04c041e43d959ee
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-15 22:23:55 +0530

    Update admin.py

--- store/admin.py
+++ store/admin.py
@@ -16,3 +16,25 @@ admin.site.register(Product)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(Profile)
 admin.site.register(ShippingAddress)
+
+from django.contrib import admin
+from .models import Product, Order, OrderItem, Profile, ShippingAddress, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name',)
+
+
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Profile)
+admin.site.register(ShippingAddress)
+
