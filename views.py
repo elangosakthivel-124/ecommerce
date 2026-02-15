069486930cb900644847be8934e5505c6c307bfd
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-15 22:25:01 +0530

    Update views.py

--- views.py
+++ views.py
@@ -136,6 +136,38 @@ ShippingAddress.objects.create(
     full_name=request.user.username,
     phone=request.POST.get('phone', ''),
     address=request.POST.get('address', '')
+
+    from django.core.paginator import Paginator
+from django.db.models import Q
+from .models import Product, Category
+
+
+@login_required
+def home(request):
+    query = request.GET.get('q')
+    cat_id = request.GET.get('category')
+
+    products = Product.objects.all()
+    categories = Category.objects.all()
+
+    if query:
+        products = products.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query)
+        )
+
+    if cat_id:
+        products = products.filter(category_id=cat_id)
+
+    paginator = Paginator(products, 8)
+    page = request.GET.get('page')
+    products = paginator.get_page(page)
+
+    return render(request, 'home.html', {
+        'products': products,
+        'categories': categories
+    })
+
 )
 
 
