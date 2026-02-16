21808a970b23b4207bce9fee04d78ac075639e33
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-16 22:18:02 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -6,3 +6,18 @@ load_dotenv()
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 SECRET_KEY = os.getenv("SECRET_KEY")
+'whitenoise.middleware.WhiteNoiseMiddleware',
+
+DEBUG = os.getenv("DEBUG") == "True"
+ALLOWED_HOSTS = ["*"]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+web: gunicorn ecommerce.wsgi
+python manage.py collectstatic
+gunicorn ecommerce.wsgi
+python manage.py createsuperuser
