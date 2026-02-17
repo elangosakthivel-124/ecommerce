6b847f15812187a90b93190e6e62e946fb5f4c71
Elango sakthivel <elangosakthivel0@gmail.com>
2026-02-17 22:23:42 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,25 +1,15 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('product/<int:id>/', views.product_detail, name='product'),
-    path('add/<int:id>/', views.add_to_cart, name='add'),
-    path('cart/', views.cart, name='cart'),
-    path('remove/<int:id>/', views.remove_cart, name='remove'),
-
-    path('register/', views.register_user, name='register'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
-    path('addqty/<int:id>/', views.add_qty, name='addqty'),
-    path('subqty/<int:id>/', views.sub_qty, name='subqty'),
-    path('checkout/', views.checkout, name='checkout'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('order/<int:id>/', views.order_detail, name='order_detail'),
-    path('payment/<int:id>/', views.payment, name='payment'),
-    path('success/<int:id>/', views.success, name='success'),
-    path('profile/', views.profile, name='profile'),
-    path('admin-orders/', views.admin_orders, name='admin_orders'),
-
-
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('register/',views.register_user,name='register'),
+    path('login/',views.login_user,name='login'),
+    path('logout/',views.logout_user,name='logout'),
+    path('add/<int:id>/',views.add_to_cart),
+    path('cart/',views.cart,name='cart'),
+    path('checkout/',views.checkout),
+    path('payment/<int:id>/',views.payment,name='payment'),
+    path('success/<int:id>/',views.success),
+    path('dashboard/',views.dashboard),
 ]
