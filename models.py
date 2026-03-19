fb576a59383fa1f024dde8f274422e175eeb6683
Elango sakthivel <elangosakthivel0@gmail.com>
2026-03-19 23:41:37 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
-from django.db import models
-from django.contrib.auth.models import User
-
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
@@ -10,20 +11,28 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
     image = models.ImageField(upload_to='products/')
-
-class Order(models.Model):
-    STATUS = (('pending','Pending'),('paid','Paid'))
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20,choices=STATUS,default='pending')
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (('pending', 'Pending'), ('paid', 'Paid'))
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS, default='pending')
+    created = models.DateTimeField(auto_now_add=True)
+
+
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order,related_name="items",on_delete=models.CASCADE)
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
+
 class Profile(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
-    phone=models.CharField(max_length=20)
-    address=models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
